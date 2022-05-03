f2889e434538084f3b48a4439c54f19024e74630
Daniel Subic <daniel.subic@eborsystems.com>
2022-05-03 21:21:45 +0930

    Unmirrored all the trials

--- mirror_trace.js
+++ mirror_trace.js
@@ -16,7 +16,7 @@
 // the images posted on github all have the same total line length and 15 segments 
 
 var materials = {
-		'mirror' : [false, true, true, true, true, true, true, true],
+		'mirror' : [false, false, false, false, false, false, false, false],
 		'file_names' : ["https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trialh3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial1.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial2.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/trial3.png", "https://raw.githubusercontent.com/rcalinjageman/mirror_trace/master/sample.png"],
 		'xstarts' : [47,	27,		40,		280,		27, 40, 280, 33],
 		'ystarts' : [256,	275,	45,		276,		275, 45, 276, 250],
